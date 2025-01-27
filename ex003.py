e49b57bd744dbc71df9258dab5b2ef73dcbab57f
Harrison W. Rodrigues <harrisonwrodrigues@gmail.com>
2025-01-27 11:37:08 -0300

    Commit: Adicionado exercício 003

---
+++ ex003.py
@@ -0,0 +1,7 @@
+# Desafio 003
+# Crie um programa que leia dois números e mostre a soma entre eles.
+
+n1=int(input('Digite o primeiro número a somar: '))
+n2=int(input('Digite o segundo número a somar: '))
+soma=n2+n1
+print('A soma dos dois número é: {}'.format(soma))
