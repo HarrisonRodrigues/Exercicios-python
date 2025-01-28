0087aa0c623d9dc069eb3019db2c0152eb001f36
Harrison W. Rodrigues <contato@harrisonrodrigues.com>
2025-01-27 22:12:21 -0300

    Commit: Adicionado exercício 002

---
+++ ex002.py
@@ -0,0 +1,3 @@
+# Escreva um programa que receba o nome do usuário dinamicamente e retorne uma mensagem de boas vindas com o nome fornecido.
+nome=input('Digite aqui o seu nome: ')
+print('Olá, {}! Seja muito bem vindo(a)!'.format(nome))
