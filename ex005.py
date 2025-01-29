f940bc2e809ecbb7d29dcd1672e7658f140c234b
Harrison W. Rodrigues <harrisonwrodrigues@gmail.com>
2025-01-29 08:53:46 -0300

    Commit: Adicionado exercício 005

---
+++ ex005.py
@@ -0,0 +1,6 @@
+# Desafio 005
+# Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor.
+
+inteiro=int(input('Digite um número inteiro: '))
+print('Analisando o valor: {}\nO seu antecessor é: {}\nE o seu sucessor é: {}'.format(inteiro, (inteiro-1), (inteiro+1)))
+
